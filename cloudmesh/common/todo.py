25bae40996b16a6f321f377c60533a8f678033e8
Gregor von Laszewski <laszewski@gmail.com>
2017-04-01 13:23:01 -0400

    chg:usr: add documentation to TODO

--- cloudmesh/common/todo.py
+++ cloudmesh/common/todo.py
@@ -1,7 +1,18 @@
+"""
+A class to call a TODO message. Typically it will just raise na exception. 
+However when using the TODO it allows to list the TODO in editors such as 
+pycharm to easier find them.
+"""
+
+
 class TODO(object):
 
     @classmethod
     def implement(cls, msg="Please implement"):
-        """temporary function to use to indicate that the code is not
-           yet implemented"""
+        """
+        Raises an exception as not implemented
+        :param msg: the message to print
+        :return: 
+        """
+        print(msg)
         raise NotImplementedError(msg)
