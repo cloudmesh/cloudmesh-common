b7d0642c91c15604ffd08f1be9bad0b7d0fc7aef
fugangwang <kevinwangfg@gmail.com>
2017-10-13 20:06:49 -0400

    adding the feature to automatically create the cloudmesh.yaml file upon installation and recover the missing file via calling cms

--- cloudmesh/__init__.py
+++ cloudmesh/__init__.py
@@ -4,4 +4,28 @@ cloudmesh name space.
 import pkg_resources
 pkg_resources.declare_namespace(__name__)
 
+import os
+import shutil
+from cloudmesh.common.util import path_expand
+from cloudmesh.common.console import Console
+from cloudmesh.common.Shell import Shell
 
+def create_cloudmesh_yaml(filename):
+    if not os.path.exists(filename):
+        path = os.path.dirname(filename)
+        if not os.path.isdir(path):
+            Shell.mkdir(path)
+        etc_path = os.path.dirname(__file__)
+        etc_file = os.path.join(etc_path, "etc", "cloudmesh.yaml")
+        to_dir = path_expand("~/.cloudmesh")
+        shutil.copy(etc_file, to_dir)
+        os.system("chmod -R go-rwx " + path_expand("~/.cloudmesh"))
+        Console.ok("~/.cloudmesh/cloudmesh.yaml created")
+
+
+def setup_yaml():
+    filename = path_expand("~/.cloudmesh/cloudmesh.yaml")
+    create_cloudmesh_yaml(filename)
+
+
+setup_yaml()
