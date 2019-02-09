edff5162031311a7bfe21b1250ab21ba070d3764
Gregor von Laszewski <laszewski@gmail.com>
2019-02-09 08:12:41 -0500

    * change message to list * make sure Parameter can also take None

--- cloudmesh/common/parameter.py
+++ cloudmesh/common/parameter.py
@@ -4,4 +4,8 @@ from hostlist import expand_hostlist
 class Parameter(object):
     @classmethod
     def expand(cls, parameter, allow_duplicates=False, sort=False):
-        return expand_hostlist(parameter, allow_duplicates=False, sort=False)
+        if parameter is None:
+            return parameter
+        else:
+            return expand_hostlist(parameter, allow_duplicates=False, sort=False)
+
