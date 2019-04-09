65fc07300f47c216c5a640d8e22628f298d9fdb5
Gregor von Laszewski <laszewski@gmail.com>
2019-04-09 01:06:15 -0400

    rename terminal command to container command to avoid conflicht in shell with the smae command name

---
+++ cloudmesh/DEBUG.py
@@ -0,0 +1,16 @@
+from cloudmesh.shell.variables import Variables
+from cloudmesh.common.util import banner
+from pprint import pformat
+from pprint import pprint
+import inspect
+
+
+# noinspection PyPep8Naming
+def VERBOSE(msg, label=None, color="BLUE", verbose=9):
+    if label is None:
+        label = inspect.stack()[1][4][0].strip().replace("VERBOSE(", "")
+        label = label.split(",")[0][:-1]
+
+    _verbose = int(Variables()["verbose"] or 0)
+    if _verbose >= verbose:
+        banner(pformat(msg), label=label, color=color)
