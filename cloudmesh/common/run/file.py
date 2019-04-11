9da0cdfe45d10074a26a2ea86b7c7887323c13b7
Gregor von Laszewski <laszewski@gmail.com>
2019-04-11 07:53:56 -0400

    add simple file based os run

---
+++ cloudmesh/common/run/file.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+from cloudmesh.common.util import readfile
+
+
+def run (command):
+    os.system (f"{command} > cmd-output")
+    content = readfile("cmd-output")
+    return content
