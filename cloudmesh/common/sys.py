72f460c2819a645a7f877f4e77ed59888815918c
Gregor von Laszewski <laszewski@gmail.com>
2019-04-19 16:02:41 -0400

    enhance sysinfo

--- cloudmesh/common/sys.py
+++ cloudmesh/common/sys.py
@@ -1,13 +1,32 @@
 import platform
-
+import sys
+import os
 
 def info():
     data = {
         'machine': platform.machine(),
         'version': platform.version(),
         'platform': platform.platform(),
-        'uname': platform.uname(),
+        'node': platform.uname().node,
+        'release': platform.uname().release,
+        'machine': platform.uname().machine,
+        'processor': platform.uname().processor,
         'system': platform.system(),
-        'processors': platform.system()
+        'processors': platform.system(),
+        'sys': sys.platform,
     }
-    return data
+    try:
+        data['user']= os.environ['USER']
+    except:
+        pass
+    try:
+        data['mac ver'] = platform.mac_ver()
+    except:
+        pass
+    try:
+        data['win ver'] = platform.win32_ver()
+    except:
+        pass
+
+
+    return dict(data)
