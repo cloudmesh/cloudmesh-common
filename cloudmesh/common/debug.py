8740a8d95dd5a61930b3992fb214d27a1d6587e3
Gregor von Laszewski <laszewski@gmail.com>
2019-05-17 06:33:13 -0400

    add line number and method name to verbose

--- cloudmesh/common/debug.py
+++ cloudmesh/common/debug.py
@@ -2,10 +2,11 @@ from cloudmesh.common.variables import Variables
 from cloudmesh.common.util import banner
 from pprint import pformat
 import inspect
+import os
 
 
 # noinspection PyPep8Naming
-def VERBOSE(msg, label=None, color="BLUE", verbose=9):
+def VERBOSE(msg, label=None, color="BLUE", verbose=9, location=True):
     """
     Prints a data structure in verbose mode
 
@@ -15,10 +16,25 @@ def VERBOSE(msg, label=None, color="BLUE", verbose=9):
     :param verbose: indicates when to print it. If verbose in cloudmesh is higher than the speified value it is printed
     :return:
     """
-    if label is None:
-        label = inspect.stack()[1][4][0].strip().replace("VERBOSE(", "")
-        label = label.split(",")[0][:-1]
 
     _verbose = int(Variables()["verbose"] or 0)
     if _verbose >= verbose:
-        banner(pformat(msg), label=label, color=color)
+
+        if label is None:
+            label = inspect.stack()[1][4][0].strip().replace("VERBOSE(", "")
+            label = label.split(",")[0][:-1]
+
+        if location:
+            cwd = os.getcwd()
+            frame = inspect.stack()[1]
+            module = inspect.getmodule(frame[0])
+            filename = module.__file__.replace(cwd, ".")
+            lineno = str(inspect.stack()[1][2]) + ":" + str(inspect.stack()[1][3])
+            print("# FILENAME:", filename, sep="")
+
+        hline = "\n" + 70 * "-" + "\n"
+
+        banner(filename + hline + lineno + hline + pformat(msg) ,
+               label=label,
+               color=color)
+
