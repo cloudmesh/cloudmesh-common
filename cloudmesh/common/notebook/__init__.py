c8463f4e12fb925f4bc4a48580c8aaf92b6bf5c7
Gregor von Laszewski <laszewski@gmail.com>
2019-07-11 06:06:04 -0400

    make the cloudmesh banner only show in ipython

--- cloudmesh/common/notebook/__init__.py
+++ cloudmesh/common/notebook/__init__.py
@@ -6,11 +6,13 @@ try:
     __IPYTHON__
     from IPython.core.display import display, HTML
     display(HTML("<style>.container { width:100% !important; }</style>"))
+
+    from pprint import pprint
+
+    from cloudmesh.common.util import banner
+
+    banner("Cloudmesh")
+
 except NameError:
     pass
 
-from pprint import pprint
-
-from cloudmesh.common.util import banner
-
-banner("Cloudmesh")
