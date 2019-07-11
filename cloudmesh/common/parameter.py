aa52f32ef6696a2e41247b63df5ec35cdceed40e
Gregor von Laszewski <laszewski@gmail.com>
2019-07-11 11:57:45 -0400

    find attributes in multiple dicts

--- cloudmesh/common/parameter.py
+++ cloudmesh/common/parameter.py
@@ -9,3 +9,9 @@ class Parameter(object):
         else:
             return expand_hostlist(parameter, allow_duplicates=False, sort=False)
 
+    @staticmethod
+    def find_attribute (name, dicts):
+        for d in dicts:
+            if name in d:
+                return d[name]
+        return None
