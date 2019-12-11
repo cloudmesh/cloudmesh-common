670ac1af4e5dd3c5ac597a70408e9a6f4f3aab38
Gregor von Laszewski <laszewski@gmail.com>
2019-12-11 10:19:21 -0500

    add the live command to Shell

--- cloudmesh/common/Shell.py
+++ cloudmesh/common/Shell.py
@@ -784,6 +784,18 @@ class Shell(object):
         """
         return platform
 
+    @classmethod
+    def live(command):
+        process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+        while True:
+            output = process.stdout.readline()
+            if output == '' and process.poll() is not None:
+                break
+            if output:
+                print
+                output.strip()
+                rc = process.poll()
+                return rc
 
     @classmethod
     def execute(cls,
