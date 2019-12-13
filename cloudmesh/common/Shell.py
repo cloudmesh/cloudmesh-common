cc7035475515fa94733d8a5fd7947b40cf8259c1
ElectricErudite <ElectricErudite@protonmail.com>
2019-12-13 17:32:25 -0500

    Simplify decorator catching args into one function

--- cloudmesh/common/Shell.py
+++ cloudmesh/common/Shell.py
@@ -25,15 +25,13 @@ import stat
 from pathlib import Path
 
 def NotImplementedInWindows(f):
-    def arg_catcher(*args):
-        def new_f():
-            if sys.platfom == "win32":
-                Console.error("The method {f.__name__} is not implemented in Windows,"
-                            " please implement, and/or submit an issue.")
-                sys.exit()
-            f(args)
-        return new_f
-    return arg_catcher
+    def new_f(*args):
+        if sys.platfom == "win32":
+            Console.error("The method {f.__name__} is not implemented in Windows,"
+                        " please implement, and/or submit an issue.")
+            sys.exit()
+        f(args)
+    return new_f
 
 
 
