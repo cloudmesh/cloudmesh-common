c5da4953c4276ffb6bb398d4ec3d63b67b99ba11
Gregor von Laszewski <laszewski@gmail.com>
2019-12-30 09:44:41 -0500

    add run and run2 to Shell

--- cloudmesh/common/Shell.py
+++ cloudmesh/common/Shell.py
@@ -1,6 +1,7 @@
 """
-A convenient method to execute shell commands and return their output. Note: that this method requires that the
-command be completely execute before the output is returned. FOr many activities in cloudmesh this is sufficient.
+A convenient method to execute shell commands and return their output. Note:
+that this method requires that the command be completely execute before the
+output is returned. FOr many activities in cloudmesh this is sufficient.
 """
 
 from __future__ import print_function
@@ -300,50 +301,6 @@ class Shell(object):
         pip_version = Shell.pip("--version").split()[1]
         return python_version_s, pip_version
 
-    @classmethod
-    def check_python(cls):
-        """
-        checks if the python version is supported
-        :return: True if it is supported
-        """
-        python_version = sys.version_info[:3]
-
-        v_string = [str(i) for i in python_version]
-
-        python_version_s = '.'.join(v_string)
-
-        if python_version[0] == 2:
-
-            print("You are running an unsupported version of python: {:}".format(python_version_s))
-
-            # python_version_s = '.'.join(v_string)
-            # if (python_version[0] == 2) and (python_version[1] >= 7) and (python_version[2] >= 9):
-
-            #    print("You are running an unsupported version of python: {:}".format(python_version_s))
-            # else:
-            #    print("WARNING: You are running an unsupported version of python: {:}".format(python_version_s))
-            #    print("         We recommend you update your python")
-
-        elif python_version[0] == 3:
-
-            if (python_version[0] == 3) and (python_version[1] >= 7) and (python_version[2] >= 0):
-
-                print("You are running a supported version of python: {:}".format(python_version_s))
-            else:
-                print("WARNING: You are running an unsupported version of python: {:}".format(python_version_s))
-                print("         We recommend you update your python")
-
-        # pip_version = pip.__version__
-        python_version, pip_version = cls.get_python()
-
-        if int(pip_version.split(".")[0]) >= 19:
-            print("You are running a supported version of pip: " + str(
-                pip_version))
-        else:
-            print("WARNING: You are running an old version of pip: " + str(
-                pip_version))
-            print("         We recommend you update your pip  with \n")
-            print("             pip install -U pip\n")
 
     @classmethod
     def check_output(cls, *args, **kwargs):
@@ -860,6 +817,116 @@ class Shell(object):
             return str(r, 'utf-8')
         else:
             return r
+
+    @staticmethod
+    def run2(command, encoding='utf-8'):
+        """
+        executes the command and returns the output as string. This command also
+        allows execution of 32 bit commands.
+
+        :param command: the program or command to be executed
+        :param encoding: encoding of the output
+        :return:
+        """
+        if platform.lower() == 'win32':
+            import ctypes
+
+            class disable_file_system_redirection:
+                _disable = ctypes.windll.kernel32.Wow64DisableWow64FsRedirection
+                _revert = ctypes.windll.kernel32.Wow64RevertWow64FsRedirection
+
+                def __enter__(self):
+                    self.old_value = ctypes.c_long()
+                    self.success = self._disable(ctypes.byref(self.old_value))
+
+                def __exit__(self, type, value, traceback):
+                    if self.success:
+                        self._revert(self.old_value)
+
+            with disable_file_system_redirection():
+                command = f"{command}"
+                r = subprocess.check_output(command,
+                                            stderr=subprocess.STDOUT,
+                                            shell=True)
+                if encoding is None or encoding == 'utf-8':
+                    return str(r, 'utf-8')
+                else:
+                    return r
+        elif platform.lower() == 'linux' or platform.lower() == 'darwin':
+            command = f"{command}"
+            r = subprocess.check_output(command,
+                                        stderr=subprocess.STDOUT,
+                                        shell=True)
+            if encoding is None or encoding == 'utf-8':
+                return str(r, 'utf-8')
+            else:
+                return r
+
+    @staticmethod
+    def cms(command, encoding='utf-8'):
+        return Shell.run("cms " + command, encoding=encoding)
+
+    @staticmethod
+    def run_timed(label, command, encoding=None, service=None):
+        """
+        runs teh command and uses the StopWatch to time it
+        :param label: name of the StopWatch
+        :param command: the command to be executed
+        :param encoding: the encoding
+        :param service: a prefix to the stopwatch label
+        :return:
+        """
+        _label = str(label)
+        print(_label, command)
+        StopWatch.start(f"{service} {_label}")
+        result = Shell.run(command, encoding)
+        StopWatch.stop(f"{service} {_label}")
+        return str(result)
+
+    @classmethod
+    def check_python(cls):
+        """
+        checks if the python version is supported
+        :return: True if it is supported
+        """
+        python_version = sys.version_info[:3]
+
+        v_string = [str(i) for i in python_version]
+
+        python_version_s = '.'.join(v_string)
+
+        if python_version[0] == 2:
+
+            print("You are running an unsupported version of python: {:}".format(python_version_s))
+
+            # python_version_s = '.'.join(v_string)
+            # if (python_version[0] == 2) and (python_version[1] >= 7) and (python_version[2] >= 9):
+
+            #    print("You are running an unsupported version of python: {:}".format(python_version_s))
+            # else:
+            #    print("WARNING: You are running an unsupported version of python: {:}".format(python_version_s))
+            #    print("         We recommend you update your python")
+
+        elif python_version[0] == 3:
+
+            if (python_version[0] == 3) and (python_version[1] >= 7) and (python_version[2] >= 0):
+
+                print("You are running a supported version of python: {:}".format(python_version_s))
+            else:
+                print("WARNING: You are running an unsupported version of python: {:}".format(python_version_s))
+                print("         We recommend you update your python")
+
+        # pip_version = pip.__version__
+        python_version, pip_version = cls.get_python()
+
+        if int(pip_version.split(".")[0]) >= 19:
+            print("You are running a supported version of pip: " + str(
+                pip_version))
+        else:
+            print("WARNING: You are running an old version of pip: " + str(
+                pip_version))
+            print("         We recommend you update your pip  with \n")
+            print("             pip install -U pip\n")
 
     @classmethod
     def live(cls, command):
