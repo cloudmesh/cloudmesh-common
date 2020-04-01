ca5fed6d9772142b38b44cbb0e68ddfc3cea1369
Gregor von Laszewski <laszewski@gmail.com>
2020-04-01 15:25:09 -0400

    switch stopwatch benchmark to tabulate, improve systeminfo
    Signed-off-by: Gregor von Laszewski <laszewski@gmail.com>

---
+++ tests/test_tabulate.py
@@ -0,0 +1,43 @@
+###############################################################
+# pip install .; pytest -v --capture=no  tests/test_printer..py::Test_printer.test_001
+# pytest -v --capture=no tests/test_printer.py
+# pytest -v  tests/test_printer.py
+###############################################################
+
+from cloudmesh.common.Printer import Printer
+from cloudmesh.common.util import HEADING
+from pprint import pprint
+import pytest
+
+@pytest.mark.incremental
+class Test_Printer:
+
+    def setup(self):
+        self.data = [
+            {
+                "name": "Gregor",
+                "address": {
+                    "street": "Funny Lane 11",
+                    "city": "Cloudville"
+                }
+            },
+            {
+                "name": "Albert",
+                "address": {
+                    "street": "Memory Lane 1901",
+                    "city": "Cloudnine"
+                }
+            }
+        ]
+
+
+    def test_001_print(self):
+        HEADING()
+        pprint (self.data)
+        table = Printer.flatwrite(self.data,
+                            sort_keys=["name"],
+                            order=["name", "address.street", "address.city"],
+                            header=["Name", "Street", "City"],)
+
+        print(table)
+        assert "Name" in str(table)
