421205c3a85d5b407f8faa792422e3672a712d4b
fugangwang <kevinwangfg@gmail.com>
2020-06-15 02:17:42 -0400

    Assign USER environment variable if running in colab. Otherwise certain modules in cloudmesh may have error, e.g., the StopWatch

--- cloudmesh/__init__.py
+++ cloudmesh/__init__.py
@@ -11,3 +11,8 @@ from cloudmesh.common.util import path_expand
 from cloudmesh.common.console import Console
 from cloudmesh.common.Shell import Shell
 
+import sys
+IN_COLAB = 'google.colab' in sys.modules
+
+if IN_COLAB:
+    os.environ["USER"] = "COLAB"
