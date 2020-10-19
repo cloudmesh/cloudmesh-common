8598219f3ba869d6380d2503155020da75751bfc
Gregor von Laszewski <laszewski@gmail.com>
2020-10-19 11:52:46 -0400

    improve setup.py

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
         "Operating System :: OS Independent",
         "Operating System :: MacOS",
         "Operating System :: Linux",
-        "Operating System :: Windows 10",
+        "Operating System :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
