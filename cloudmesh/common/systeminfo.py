d7caaaf7d34c09dfa57b96fd8d7c1e18a4352226
J.P <70083705+jpfleischer@users.noreply.github.com>
2022-05-06 01:07:13 -0400

    Update systeminfo.py

--- cloudmesh/common/systeminfo.py
+++ cloudmesh/common/systeminfo.py
@@ -128,7 +128,7 @@ def is_local(host):
     """
     Checks if the host is the localhost
 
-    :param host: The hotsname or ip
+    :param host: The hostname or ip
     :return: True if the host is the localhost
     """
     return host in ["127.0.0.1",
