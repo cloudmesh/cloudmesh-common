1633292b64be2f660fc668101704e01e8b6d43e2
J.P <70083705+stapmoshun@users.noreply.github.com>
2022-07-15 09:38:52 -0400

    fix browser test

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -12,7 +12,8 @@ from cloudmesh.common.Shell import Shell
 from cloudmesh.common.util import HEADING
 from cloudmesh.common.Benchmark import Benchmark
 
-import time
+from pathlib import Path
+import os
 
 
 class TestBrowser:
@@ -20,38 +21,21 @@ class TestBrowser:
     def test_shell_browser(self):
         HEADING()
         Benchmark.Start()
+        dir = os.path.join(Path.home(), 'cm/cloudmesh-common/tests/test.svg')
         # Shell.copy("test-graphviz.svg", '/tmp/test-graphviz.svg')
         # Shell.copy("test-graphviz.svg", "~/test-graphviz.svg")
         # r = Shell.browser("~/test-graphviz.svg")
         # Shell.copy("test-graphviz.svg", f"{Path.home()}/test-graphviz.svg")
-        r = Shell.browser(f'http://google.com')
-        print('testing unsecured')
-        time.sleep(3)
         r = Shell.browser(f'https://google.com')
-        print('testing secured')
-        time.sleep(3)
-        r = Shell.browser(f"C:/Users/abeck/cm/cloudmesh-cc/test-graphviz.svg")
-        print('i just tried a fullpath')
-        time.sleep(3)
-        r = Shell.browser(f"file:///C:/Users/abeck/cm/cloudmesh-cc/test-graphviz.svg")
-        print('i just tried with file:')
-        time.sleep(3)
-        r = Shell.browser(f"~/test-graphviz.svg")
-        print('i just opened the home dir and the svg in there')
-        time.sleep(5)
-        r = Shell.browser(f'test-graphviz.svg')
-        print('i just tried no slashes')
-        time.sleep(5)
-        r = Shell.browser(f'./test-graphviz.svg')
-        print('i just tried something wacky')
-        time.sleep(5)
+        r = Shell.browser(dir)
+        r = Shell.browser(f"file:///{dir}")
+        r = Shell.browser(f"~/cm/cloudmesh-common/tests/test.svg")
+        r = Shell.browser(f'tests/test.svg')
+        #r = Shell.browser(f'./test.svg')
         # assert r == path_expand(f'~/test-graphviz.svg')
         # input()
         # r = Shell.browser("file://~/test-graphviz.svg")
-        # r = Shell.browser("test-graphviz.svg")
         # r = Shell.browser("file://test-graphviz.svg")
         # r = Shell.browser("file://tmp/test-graphviz.svg")
-        # r = Shell.browser("http://google.com")
-        # r = Shell.browser("https://google.com")
         print(r)
         Benchmark.Stop()
