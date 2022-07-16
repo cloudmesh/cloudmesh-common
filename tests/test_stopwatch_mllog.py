10d88e8d0220d4db49fb32b6aa52a3dd50a83b06
Robert Knuuti <uqq5zz@virginia.edu>
2022-07-15 20:12:01 -0400

    Added preliminary test case for stopwatch.py testing.

--- tests/test_stopwatch_mllog.py
+++ tests/test_stopwatch_mllog.py
@@ -4,12 +4,26 @@
 # pytest -v  tests/test_stopwatch.py
 ###############################################################
 
+from typing import Dict, Any
+import json
 import time
 
 import pytest
 from cloudmesh.common.StopWatch import StopWatch
 
 from cloudmesh.common.util import HEADING
+
+from io import StringIO  # Python 3
+import sys
+
+
+def convert_mllog_to_object(log_stream: StringIO):
+    obj = list()
+    text = log_stream.getvalue().replace(":::MLLOG ", "")
+    for x in text.split("\n"):
+        if x != "":
+            obj.append(json.loads(x))
+    return obj
 
 
 @pytest.mark.incremental
@@ -77,13 +91,38 @@ class Test_Printer:
 
         assert t >= n * dt
 
-
     def test_stopwatch_event(self):
         HEADING()
         data = {"a": 1}
         t = StopWatch.event("stopwtch event", msg=data)
 
-
     def test_print(self):
         StopWatch.benchmark(sysinfo=True, csv=True, sum=True, tag="pytest")
         assert True
+
+    def test_stopwatch_log_event(self):
+        HEADING()
+        # Create the in-memory "file"
+        temp_out = StringIO()
+        # Replace default stdout (terminal) with our stream
+        sys.stdout = temp_out
+        data = {"a": 1}
+        StopWatch.activate_mllog()
+        t = StopWatch.log_event(values=data)
+        sys.stdout = sys.__stdout__
+        log_text = temp_out.getvalue()
+        assert ':::MLLOG' in log_text, 'logger must have mllog entries when activated'
+
+        log_object = convert_mllog_to_object(temp_out)
+        import pprint
+        pprint.pprint(log_object)
+        assert all(list(map(lambda x: x['namespace'] == "cloudmesh", log_object))), "Logger must have cloudmesh namespace."
+
+        assert (any(list(map(lambda x: x['key'] == "mllog-start-values", log_object))) and
+                any(list(map(lambda x: x['key'] == "mllog-stop-values", log_object))) and
+                any(list(map(lambda x: x['key'] == "mllog-event-values", log_object)))), "Logger event keys must apply the key label across all logs"
+
+        for x in log_object:
+            if x['event_type'] == "POINT_IN_TIME":
+                assert isinstance(x['value'], str), "Value must be converted to string."
+                assert x['value'] == str(data), "Event data must capture passed values in string format"
