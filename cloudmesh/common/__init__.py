2cddc42896931d31bff0f5c6fe29a83496460d10
Gregor von Laszewski <laszewski@gmail.com>
2022-08-03 11:50:37 -0400

    Bump version: 4.3.158 → 4.3.159

--- cloudmesh/common/__init__.py
+++ cloudmesh/common/__init__.py
@@ -20,4 +20,4 @@ from cloudmesh.common.dotdict import dotdict
 from cloudmesh.common.FlatDict import FlatDict
 from cloudmesh.common.variables import Variables
 
-version = '4.3.158'
+version = '4.3.159'
