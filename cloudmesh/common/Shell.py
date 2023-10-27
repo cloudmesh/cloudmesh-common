5756b6638da0086001bb219b482c8bbb772e4935
J.P <70083705+stapmoshun@users.noreply.github.com>
2023-10-27 04:27:31 -0400

    fix brew install

--- cloudmesh/common/Shell.py
+++ cloudmesh/common/Shell.py
@@ -593,7 +593,7 @@ class Shell(object):
         print('test')
         
         # command = 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
-        command = f'osascript -e \'tell application "Terminal" to do script "/bin/bash -c \\"export SUDO_ASKPASS={askpass} ; export NONINTERACTIVE=1 ; $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh) ; exit \\""\''
+        command = f'osascript -e \'tell application "Terminal" to do script "/bin/bash -c \\"export SUDO_ASKPASS={askpass} ; export NONINTERACTIVE=1 ; $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\\""\''
         print(command)
         # try:
         subprocess.run(command, shell=True, check=True)
