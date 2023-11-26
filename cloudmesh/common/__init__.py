b27aa7ea881400dc5f1a00ea5804d240b1d494be
Gregor von Laszewski <laszewski@gmail.com>
2023-11-26 10:25:54 -0500

    Bump version: 5.0.1 → 5.0.2

--- cloudmesh/common/__init__.py
+++ cloudmesh/common/__init__.py
@@ -20,4 +20,4 @@ from cloudmesh.common.dotdict import dotdict
 from cloudmesh.common.FlatDict import FlatDict
 from cloudmesh.common.variables import Variables
 
-version = '5.0.1'
+version = '5.0.2'
