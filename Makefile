9138cd7e891a7bb490c0134cd6f17d2394e5e080
Gregor von Laszewski <laszewski@gmail.com>
2023-12-15 05:26:27 -0500

    simplify makefile

--- Makefile
+++ Makefile
@@ -1,12 +1,6 @@
 package=common
-UNAME=$(shell uname)
-VERSION=`head -1 VERSION`
 
-include makefile-banner.mk
-
-source:
-	$(call banner, "Install cloudmesh-${package}")
-	pip install -e . -U
+include makefile-basic.mk
 
 include makefile-test.mk
 
--- makefile-basic.mk
+++ makefile-basic.mk
@@ -1,3 +1,6 @@
+UNAME=$(shell uname)
+VERSION=`head -1 VERSION`
+
 define banner
 	@echo
 	@echo "############################################################"
@@ -5,3 +8,6 @@ define banner
 	@echo "############################################################"
 endef
 
+source:
+	$(call banner, "Install cloudmesh-${package}")
+	pip install -e . -U
