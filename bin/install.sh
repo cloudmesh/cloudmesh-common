9688578fb2cb5d39fddd1e75a4335b8e6e80baee
Gregor von Laszewski <laszewski@gmail.com>
2023-12-16 17:40:02 -0500

    use black to reformat the code

--- bin/install.sh
+++ bin/install.sh
@@ -3,10 +3,9 @@
 #cd ..
 cd ../cloudmesh-common; make dist; make local
 cd ../cloudmesh-cmd5; make dist; make local
-#cd ../cloudmesh-bumpversion; make dist; make local
+cd ../cloudmesh-bumpversion; make dist; make local
 cd ../cloudmesh-sys; make dist; make local
-
-#cd ../cloudmesh-bar; make dist; make local
+cd ../cloudmesh-bar; make dist; make local
 cd ../cloudmesh-common
 #cms info
 #cms info path
